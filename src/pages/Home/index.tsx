c3ad6abcc0f739610558658fc37383ba7d5fe883
Junior de Oliveira <ellismarjr@hotmail.com>
2021-12-06 09:47:58 -0300

    chore: add Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,74 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {FlatList} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import {Cart} from '../../components/Cart';
+import {FruitItem, IFruitItem} from '../../components/FruitItem';
+import {api} from '../../services/api';
+import {formatPrice} from '../../utils/formatPriceBR';
 
-import {Container, Title, Header, SearchInput, SearchButton} from './styles';
+import {
+  Container,
+  TitleContainer,
+  Title,
+  TitleWrapper,
+  Subtitle,
+  Header,
+  SearchInput,
+  SearchButton,
+} from './styles';
+
+export interface Fruit extends IFruitItem {
+  id: number;
+}
 
 export function Home() {
+  const [fruits, setFruits] = useState<Fruit[]>([]);
+
+  useEffect(() => {
+    const getFruits = async () => {
+      const response = await api.get('/fruits');
+
+      const responseFormatted = response.data.map((fruit: Fruit) => {
+        return {
+          ...fruit,
+          price: formatPrice(fruit.price),
+        };
+      });
+      setFruits(responseFormatted);
+    };
+
+    getFruits();
+  });
+
   return (
-    <Container>
-      <Title>FrameCommerce</Title>
-      <Header>
-        <SearchInput />
-        <SearchButton>
-          <Feather name="search" size={24} color="#FFF" />
-        </SearchButton>
-      </Header>
-    </Container>
+    <>
+      <Container>
+        <TitleContainer>
+          <Feather name="shopping-cart" size={24} color="#E83F5B" />
+          <TitleWrapper>
+            <Title>Frame</Title>
+            <Subtitle>Commerce</Subtitle>
+          </TitleWrapper>
+        </TitleContainer>
+
+        <Header>
+          <SearchInput />
+          <SearchButton activeOpacity={0.8}>
+            <Feather name="search" size={24} color="#FFF" />
+          </SearchButton>
+        </Header>
+
+        <FlatList
+          data={fruits}
+          keyExtractor={item => String(item.id)}
+          renderItem={({item}) => <FruitItem data={item} />}
+          showsVerticalScrollIndicator={false}
+          horizontal={false}
+          numColumns={2}
+          columnWrapperStyle={{flex: 1, justifyContent: 'space-between'}}
+        />
+      </Container>
+      <Cart />
+    </>
   );
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,13 +1,33 @@
+import {FlatList} from 'react-native';
 import styled from 'styled-components/native';
+import {Fruit} from '.';
 
 export const Container = styled.View`
   flex: 1;
 
-  padding: 30px;
+  padding: 30px 30px 0;
+`;
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TitleWrapper = styled.View`
+  flex-direction: row;
 `;
 
 export const Title = styled.Text`
   font-size: 20px;
+  text-align: center;
+  color: #3d3d4d;
+  margin-left: 10px;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
   text-align: center;
   color: #3d3d4d;
 `;
@@ -16,12 +36,13 @@ export const Header = styled.View`
   flex-direction: row;
   align-items: center;
   margin-top: 20px;
+  margin-bottom: 20px;
 `;
 
 export const SearchInput = styled.TextInput`
   flex: 1;
-  background: #3d3d4d;
-  opacity: 0.1;
+  background: #c4c4c4;
+  color: #3d3d4d;
 
   border-radius: 5px;
 
@@ -34,3 +55,7 @@ export const SearchButton = styled.TouchableOpacity`
   padding: 10px;
   border-radius: 5px;
 `;
+
+export const FruitsList = styled(FlatList as new () => FlatList<Fruit>).attrs({
+  showsVerticalScrollIndicator: false,
+})``;
