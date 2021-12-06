e291a1d5cfd08b91d749b49f3ac5810f617bc879
Junior de Oliveira <ellismarjr@hotmail.com>
2021-12-06 11:19:49 -0300

    refactor: header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,41 @@
+import {useNavigation} from '@react-navigation/core';
+import React, {useCallback} from 'react';
+
+import Feather from 'react-native-vector-icons/Feather';
+
+import {
+  TitleContainer,
+  BackButton,
+  TitleWrapper,
+  Title,
+  Subtitle,
+} from './styles';
+
+interface Props {
+  isHomePage?: boolean;
+}
+
+export function Header({isHomePage = true}: Props) {
+  const navigation = useNavigation();
+
+  const handleGoBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  return (
+    <TitleContainer>
+      {isHomePage ? (
+        <Feather name="shopping-cart" size={24} color="#E83F5B" />
+      ) : (
+        <BackButton activeOpacity={0.7} onPress={handleGoBack}>
+          <Feather name="arrow-left" size={24} color="#E83F5B" />
+        </BackButton>
+      )}
+
+      <TitleWrapper>
+        <Title>Frame</Title>
+        <Subtitle>Commerce</Subtitle>
+      </TitleWrapper>
+    </TitleContainer>
+  );
+}
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,28 @@
+import styled from 'styled-components/native';
+
+export const TitleContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const TitleWrapper = styled.View`
+  flex-direction: row;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  text-align: center;
+  color: #3d3d4d;
+  margin-left: 10px;
+`;
+
+export const Subtitle = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  text-align: center;
+  color: #3d3d4d;
+`;
