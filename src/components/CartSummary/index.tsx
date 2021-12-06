153ea7cdff426be93abf7f1582ad601f2f3b7e82
Junior de Oliveira <ellismarjr@hotmail.com>
2021-12-06 11:20:15 -0300

    chore: navigate to Cart

--- src/components/CartSummary/index.tsx
+++ src/components/CartSummary/index.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import {useNavigation} from '@react-navigation/core';
+import React, {useCallback} from 'react';
 import Feather from 'react-native-vector-icons/Feather';
 
 import {Container, ShowItemsButtom, AmountItems, Amount} from './styles';
 
 export function CartSummary() {
+  const navigation = useNavigation();
+
+  const handleNavigateToCart = useCallback(() => {
+    navigation.navigate('Cart');
+  }, [navigation]);
+
   return (
     <Container>
-      <ShowItemsButtom activeOpacity={0.7}>
+      <ShowItemsButtom activeOpacity={0.7} onPress={handleNavigateToCart}>
         <Feather name="shopping-cart" size={24} color="#fff" />
         <AmountItems>2 itens</AmountItems>
       </ShowItemsButtom>
